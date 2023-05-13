c5a30443fae8972fc3ba93fdb348ae7adae93338
Carlos Aurelio Briceño Gomez <aurel.gomez@outlook.com>
2023-05-13 01:13:24 -0500

    ultimate check before develop front

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -11,7 +11,6 @@ import { CreateUserDto } from './user.dto';
 import to from 'await-to-js';
 import { UseZodGuard } from 'nestjs-zod';
 import { CreateUserSchema } from './user.zod';
-import { MessagePattern } from '@nestjs/microservices';
 
 @Controller('user')
 export class UserController {
@@ -36,11 +35,5 @@ export class UserController {
       message: 'User created successfully',
       data: user,
     };
-  }
-
-  @MessagePattern({ cmd: 'sum' })
-  accumulate(data: number[]): number {
-    console.log('data', data);
-    return (data || []).reduce((a, b) => a + b);
   }
 }
